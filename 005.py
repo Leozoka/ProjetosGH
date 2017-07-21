838a2278fac06a24d8f7be9a5142ee58e9b43d84
Leozoka <leo.neves8@hotmail.com>
2017-07-21 20:01:04 -0300

    Exercicío 05..

---
+++ 005.py
@@ -0,0 +1,5 @@
+cont1 = 2 + 3 * 5
+cont2 = (2 + 3) * 5
+print(cont1)
+print(cont2)
+#O resultado muda por causa da ordem de precedência. Os parentêses são a primeira precêdencia, logo após vem o que está fora dele.
