7f63bb5e90214ad78cae4acdfe37c0f7da7dd08f
Leozoka <leo.neves8@hotmail.com>
2017-07-21 21:29:13 -0300

    Exercicío 08..

---
+++ 008.py
@@ -0,0 +1,3 @@
+colors = ['red', 'green', 'purple', 'black', 'white']
+print(colors[0])
+#Para acessar a um item da lista que deseja, deve-se escrever o nome da lista seguido pelo índice do item entre colchetes.
