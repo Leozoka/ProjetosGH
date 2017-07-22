3ba71401b38cc8c2851ebd775e125eba4afb93a9
Leozoka <leo.neves8@hotmail.com>
2017-07-21 21:38:25 -0300

    Exercicío 10..

---
+++ 010.py
@@ -0,0 +1,3 @@
+colors = ['yellow', 'white', 'green']
+print(colors[-1].title())
+#Muitas vezes você quer acessar a lista sem saber exatamente seu tamanho, com isso pode pegar a última palavra com o índice negativo.
