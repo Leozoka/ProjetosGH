94b4d7dc4d15ea50bd179af896e15bf7b881706a
Leozoka <leo.neves8@hotmail.com>
2017-07-23 11:20:38 -0300

    Exercicío 020..

---
+++ 020.py
@@ -0,0 +1,7 @@
+colors = ['red', 'green', 'white']
+color = 'green'
+colors.remove(color)
+print(colors)
+print('The ' + color.title() + ' color.')
+
+#Porque o método remove() exclui apenas a primeira ocorrência do valor que você deseja. Caso o valor apareça mais vezes, você deverá usar um loop.
