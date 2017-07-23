7b8bd6c3cd88e179aa137d96bc1be952570bfa70
Leozoka <leo.neves8@hotmail.com>
2017-07-23 11:19:05 -0300

    Exercicío 019..

---
+++ 019.py
@@ -0,0 +1,3 @@
+# 19 - Explique quando usar o del e quando usar o pop()?
+# R: Utiliza-se del quando se quer exluir pemanentemente um item desejado de sua lista.
+# R: Com o método pop() é pra quando quiser usar o item removido novamente.
