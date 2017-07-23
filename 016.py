d227b8339369322b982282d83ccc7fbefdc11d0c
Leozoka <leo.neves8@hotmail.com>
2017-07-23 11:17:12 -0300

    Exercicío 016..

---
+++ 016.py
@@ -0,0 +1,8 @@
+colors = ['red', 'green', 'blue']
+print(colors)
+
+popped_colors = colors.pop()
+print(colors)
+print(popped_colors)
+
+#Método pop(), é utilizado para remover o último item de uma lista, mas ainda permite que possa trabalhar com este item.
