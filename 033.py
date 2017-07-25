719c7b2dddc2048ddd7848d797a854fbbfa13d83
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:31:08 -0300

    Exercicío 033..

---
+++ 033.py
@@ -0,0 +1,5 @@
+jogadores = ['Ana', 'Beatriz', 'Eduardo']
+for jogador in jogadores:
+print(jogador.title() + ', foi um bom jogo!')
+
+#Esse erro ocorre, porque o Python usa indentação para saber quando uma linha de código está conectada a linha acima.
