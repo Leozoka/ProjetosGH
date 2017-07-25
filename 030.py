61ba7aba2b932934bce5c8fda46947653d67157b
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:29:28 -0300

    Exercicío 04..

---
+++ 030.py
@@ -0,0 +1,6 @@
+letras = ['a', 'b', 'c', 'd', 'e']
+for letra in letras:
+    print(letra)
+
+#Utilizamos loop para quando desejamos executar a mesma entrada em uma lista, fazendo que o processo se repita.
+#Utilizar palavras no singular e no plural pode ajudar.
