cf0ba0a39cdb29d7ae7a9cc06953baac418df994
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:29:55 -0300

    Exercicío 031..

---
+++ 031.py
@@ -0,0 +1,5 @@
+jogadores = ['Ana', 'Beatriz', 'Eduardo']
+for jogador in jogadores:
+    print(jogador.title() + ', foi um bom jogo!')
+
+#Com o loop podemos deixar uma mensagem para cada item da lista, nâo precisando utilizar print() várias vezes.
