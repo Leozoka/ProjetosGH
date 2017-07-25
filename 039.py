7cc87b09802874f2450d81e24516e63436fedf3f
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:33:07 -0300

    Exercicío 04..

---
+++ 039.py
@@ -0,0 +1,4 @@
+for value in range(1, 5):
+    print(value)
+
+#A função range(), faz com que o Python comece a contar no primeiro valor que você atribuiu e para de contar assim que atinge o segundo valor que você fornece.
