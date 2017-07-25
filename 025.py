d62588239183a1e58604633d7d407ed63e3fb65a
Leozoka <leo.neves8@hotmail.com>
2017-07-24 21:28:01 -0300

    Exercicío 025..

---
+++ 025.py
@@ -0,0 +1,9 @@
+games = ['crash', 'sonic', 'fifa', 'gow']
+games.reverse()
+print(games)
+
+#O modo reverse() altera a lista com os elemento de trás para frente, não coloca em ordem alfabética.
+#Para voltar a lista original, basta fazer o mesmo processo.
+
+games.reverse()
+print(games)
