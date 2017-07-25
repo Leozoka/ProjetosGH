18f9f064795f48d42d6c33dddd08b94e8741b095
Leozoka <leo.neves8@hotmail.com>
2017-07-24 21:26:33 -0300

    Exercicío 024..

---
+++ 024.py
@@ -0,0 +1,9 @@
+colors = ['green', 'yellow', 'red', 'white']
+print('Está é a lista: ')
+print(colors)
+
+print('\nEstá é a lista em ordem alfabética: ')
+print(sorted(colors))
+
+print('\nEstá é a lista novamente: ')
+print(colors)
