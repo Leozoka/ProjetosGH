a699a73841457df4f783c6516a54ee754f7515fa
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:30:47 -0300

    Exercicío 032..

---
+++ 032.py
@@ -0,0 +1,4 @@
+jogadores = ['Ana', 'Beatriz', 'Eduardo']
+for jogador in jogadores:
+    print(jogador.title() + ', foi um bom jogo!')
+    print('Vamos nos ver no próximo jogo, ' + jogador.title() + '.\n')
