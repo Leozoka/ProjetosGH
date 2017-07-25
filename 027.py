f0b5227e61a2a5127d0ae6ce539b252e037d7932
Leozoka <leo.neves8@hotmail.com>
2017-07-24 21:29:38 -0300

    Exercicío 027..

---
+++ 027.py
@@ -0,0 +1,5 @@
+colors = ['white', 'black', 'red']
+elem1 = len(colors)
+elem2 = 'white'
+print('Quantidade de palavras na lista: ' + str(elem1))
+print('Quantidade de caracteres em alguma das palavras: ' + str(len(elem2)))
