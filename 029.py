34059dbdea38a0f2d2d7c793e69d1d9e97df23d2
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:27:39 -0300

    Exercicío 029..

---
+++ 029.py
@@ -0,0 +1,9 @@
+lista = ['fusca', 'camaro', 'bmw', 'uno', 'ferrari', 'chevete', 'audi']
+print('Vendedor: Seja bem vindo! Em que posso lhe ajudar?')
+print('Comprador: Olá, poderia me informar quantos carros tem na concessionária?')
+print('Vendedor: No momento temos', len(lista), 'carros.')
+print('Comprador: Quais são os modelos?')
+print('Vendedor: Temos estes modelos: ' + lista[0].title() + ', ' + lista[1].title() + ', ' + lista[2].title() + ', ' + lista[3].title() + ', ' +  lista[4].title() + ', ' + lista[5].title() + ', ' + lista[6].title() + '.')
+print('Comprador: Gostei do modelo, ' + lista[2].title() + '. Qual o preço ?')
+print('Vendedor: O preço deste modelo é R$ 153.391,16.')
+print('Comprador: Está barato! *risos*')
