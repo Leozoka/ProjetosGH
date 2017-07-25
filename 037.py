5000afd481fbcdfdbb0535ccf915b6a37315f914
Leozoka <leo.neves8@hotmail.com>
2017-07-25 15:32:28 -0300

    Exercicío 037..

---
+++ 037.py
@@ -0,0 +1,4 @@
+#Explique o Colon, quando devo usar?
+
+#O Colon diz ao Python para interpretar a próxima linha como o início de um Loop.
+#Caso esqueça o Colon, você terá um erro de sintaxe, porque o Python não sabe o que você está tentando fazer.
