66ebac0ea2d8480a02760090a7a0db60003a5350
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:37:57 -0300

    Exercicío 049..

---
+++ 049.py
@@ -0,0 +1,5 @@
+doces = ['bolo', 'brigadeiro', 'pudim', 'torta']
+print(doces[2:])
+
+#Sintaxe semelhante ao execicío 049.
+#Se você quiser todos os itens, a partir do terceiro item, você pode começar do 2 e omitir o segundo índice. O Python retorna todos os itens pedidos até o final da lista.
