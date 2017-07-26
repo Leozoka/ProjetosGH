cc910e0280d52f330bd646f2dfdb4399c094fd7a
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:22:55 -0300

    Exercicío 051..

---
+++ 051.py
@@ -0,0 +1,6 @@
+my_cars = ['fusca', 'camaro', 'chevete']
+friend_cars = my_cars[:]
+print('Meus carros favoritos são: ')
+print(my_cars)
+print('\nCarros favoritos do meu amigo: ')
+print(friend_cars)
