299a25a52cef3515e24628ecb46c8ef0dfe527a8
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:23:41 -0300

    Exercicío 052..

---
+++ 052.py
@@ -0,0 +1,10 @@
+my_cars = ['fusca', 'camaro', 'chevete']
+friend_cars = my_cars[:]
+my_cars.append('lancer')
+friend_cars.append('uno')
+print('Meus carros favoritos são: ')
+print(my_cars)
+print('\nCarros favoritos do meu amigo: ')
+print(friend_cars)
+
+#Para provar que temos duas listas separadas, adicionamos um novo item em cada variavél, usando o fatiamento.
