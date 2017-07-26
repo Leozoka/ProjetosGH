a02aaa84db74b677d58b8e68a3c69614d5faad38
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:36:15 -0300

    Exercicío 046..

---
+++ 046.py
@@ -0,0 +1,5 @@
+doces = ['bolo', 'brigadeiro', 'pudim', 'torta']
+print(doces[0:3])
+
+#Para fazer o fatiamento você especifica o índice do primeiro e último elemento que você quer exibir (parecido com a função range()).
+#Para produzir os três primeiros elementos em uma lista, você solicitaria os índices de 0 a 3, que retornaria os elementos.
