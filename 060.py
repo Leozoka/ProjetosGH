724b5ea9e51d2882c63313ad9a45982fd8a7255e
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:29:54 -0300

    Exercicío 060..

---
+++ 060.py
@@ -0,0 +1,4 @@
+# 60 - Explique sobre Indentação.
+# PEP 8 recomenda que você use quatro espaços por nível de indentação. Usando quatro espaços melhoram a legibilidade, deixando espaço para  vários níveis de indentação em cada linha.
+# Em um documento de texto as pessoas utilizam guias em vez de espaços para recuar, mas o intérprete de Python fica confuso quando as abas são misturadas com espaços.
+# A mistura de abas e espaços no seu arquivo pode causar problemas que são difíceis de diagnosticar. Se tem uma mistura de abas e espaços, pode converter todas as guis em um arquivo na maioria dos editores.
