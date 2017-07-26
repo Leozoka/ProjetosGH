b6edb6f84e9aa4825aaf5a83098751429d3c02db
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:28:19 -0300

    Exercicío 057..

---
+++ 057.py
@@ -0,0 +1,6 @@
+tamanhos = (548, 5848, 87)
+tamanho[0] = 40
+print(tamanho)
+
+# O código em que você tenta alterar o valor, acontece um erro, porque estamos tentando alterar uma tupla, que não pode ser feito.
+# Pyhton diz que não podemos atribuir um novo valor para um item em uma tupla.
