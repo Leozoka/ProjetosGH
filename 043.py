bde2f6f02ec9b0565508cfc366d9c3fab08b7833
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:31:05 -0300

    Exercicío 043..

---
+++ 043.py
@@ -0,0 +1,7 @@
+var1 = []
+for value in range(1, 11):
+    var1.append(value ** 2)
+
+print(var1)
+
+#Omitimos o quadrado variavél que era nossa segunda variavél (var2), e adicionamos o valor diretamente na lista.
