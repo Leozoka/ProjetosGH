d8a05bac6c8e06ce09051fc07a05c3602b83e5b6
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:36:54 -0300

    Exercicío 047..

---
+++ 047.py
@@ -0,0 +1,4 @@
+doces = ['bolo', 'brigadeiro', 'pudim', 'torta']
+print(doces[1:4])
+
+#Podemos gerar qualquer subconjunto de uma lista. Nesse exercicío pegamos do segundo ao quarto item, caso fosse trabalhar com o primeiro item teria que usar o índice [0:4] ou [:4].
