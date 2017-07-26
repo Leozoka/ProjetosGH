94644bf18e38b16b2896af42b94c1c1c1ea63b80
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:29:32 -0300

    Exercicío 059..

---
+++ 059.py
@@ -0,0 +1,12 @@
+tamanhos = (252, 784)
+print('Tamanhos da parede: ')
+for tamanho in tamanhos:
+    print(tamanho)
+tamanhos= (504, 1568)
+print('\nTamanhos do terreno: ')
+for tamanho in tamanhos:
+    print(tamanho)
+
+# Você não pode modificar uma Tupla, você atribuir um novo valor a uma nova variavél que contém uma Tupla.
+# Python não reconheceu nenhum erro desta vez, porque substituir uma variavél é válida.
+# Quando comparados com listas, as Tuplas são estruturas de dados simples. Usa-se quando quiser armazenar um conjunto de valores que não devem ser alterados ao longo de um programa.
