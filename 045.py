c469f88f1dfd631bae202fd757e5255197c2b39b
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:35:36 -0300

    Exercicío 045..

---
+++ 045.py
@@ -0,0 +1,5 @@
+quadrado = [value ** 2 for value in range(1, 11)]
+print(quadrado)
+
+#As Listas de Compreensões permite gerar uma linha de código, ao invés de utilizar 3 ou mais linhas.
+#Permite que crie um Loop e a criação de novos elementos em uma linha.
