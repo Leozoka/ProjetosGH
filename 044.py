61aeb40c5e68656d8b6fe8b0224009c9468f6ee7
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:34:28 -0300

    Exercicío 044..

---
+++ 044.py
@@ -0,0 +1,7 @@
+números = [7, 8, 9, 2, 6, 0, 87, 100]
+minimo = min(números)
+maximo = max(números)
+soma = sum(números)
+print('Número mínimo: {}\nNúmero máximo: {}\nSoma: {}'.format(minimo, maximo, soma))
+
+#Estas são algumas funções específicas para listas de números. Neste exemplo podemos encontrar o número mínimo, máximo e a soma de todos os números.
