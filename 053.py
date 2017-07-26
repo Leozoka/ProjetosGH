21430aaa9d4bf5e3145cb95a87537c0510d76054
Leozoka <leo.neves8@hotmail.com>
2017-07-26 17:24:07 -0300

    Exercicío 053..

---
+++ 053.py
@@ -0,0 +1,10 @@
+my_cars = ['fusca', 'camaro', 'chevete']
+friend_cars = my_cars
+my_cars.append('lancer')
+friend_cars.append('uno')
+print('Meus carros favoritos são: ')
+print(my_cars)
+print('\nCarros favoritos do meu amigo: ')
+print(friend_cars)
+
+#Python faz a nova variavél (friend_car) se juntar com a outra variavél (my_cars). Então ambas variavéis apontam para a mesma lista.
