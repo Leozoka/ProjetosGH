be156c4b70f41b5596f8070a7ab6e0cd468271ea
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:38:26 -0300

    Exercicío 050..

---
+++ 050.py
@@ -0,0 +1,4 @@
+doces = ['bolo', 'brigadeiro', 'pudim', 'torta']
+print(doces[-3:])
+
+#Esta sintaxe permite que você emita todos os elementos de qualquer ponto em sua lista até o final. O índice retorna um elemento a uma certa distância do final de uma lista, você pode enviar qualquer fatia do final de uma lista.
