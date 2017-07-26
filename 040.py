da21a015cf19709ffca18fd2e9dfd9c5317d3bed
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:28:00 -0300

    Exercicío 040..

---
+++ 040.py
@@ -0,0 +1,4 @@
+numeros = list(range(1, 6))
+print(numeros)
+
+#Se quiser fazer uma lista de números, você pode converter os resultados do intervalo(), diretamente em uma lista usando a função list(). Quando envolve a lista() em torno de uma função range(), a saída será um lista de números.
