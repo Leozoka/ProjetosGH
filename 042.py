6f81a43c22a5963a19821f319f77786a503b57f4
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:30:32 -0300

    Exercicío 042..

---
+++ 042.py
@@ -0,0 +1,7 @@
+var1 = []
+for value in range(1, 11):
+    var2 = value ** 2
+    var1.append(var2)
+
+print(var1)
+#Começa com uma lista vazia, depois pedimos ao Python para encaminhar a range(1, 10). Dentro do Loop o valor é aumentado para a segunda potência e fica armazenado na segunda variavél (var2), cada novo valor é anexado na lista.
