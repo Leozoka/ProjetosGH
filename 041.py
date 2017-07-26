c0301ed8c191ba03cc31f30e40ecfce0b543e414
Leozoka <leo.neves8@hotmail.com>
2017-07-26 14:29:59 -0300

    Exercicío 041..

---
+++ 041.py
@@ -0,0 +1,4 @@
+pares = list(range(2, 10, 2))
+print(pares)
+
+#Neste exemplo a função range() começa com o valor 2 e em seguida adiciona 2 a esse valor. Ele adiciona repetidamente até atingir ou chegar no final.
