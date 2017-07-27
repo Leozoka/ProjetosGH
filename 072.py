adb8fffe41777673768fcfba3cad6e13c6248551
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:58:24 -0300

    Exercicío 072..

---
+++ 072.py
@@ -0,0 +1,16 @@
+# Exercicío 070:
+idade1 = '32'
+idade2 = '28'
+if idade1 >= '31' or idade2 >= '31':
+    print('True!')
+else:
+    print('False!')
+
+# Exercicío 071:
+idade1 = '32'
+idade2 = '28'
+if idade1 >= '29' or idade2 >= '29':
+    print('True!')
+else:
+    print('False!')
+# Utilizando (or), você também pode verificar duas condições.
