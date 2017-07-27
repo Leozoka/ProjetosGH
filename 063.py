05dba0790f35c422c91dda83d70b53ab53eb21f6
Leozoka <leo.neves8@hotmail.com>
2017-07-26 22:11:14 -0300

    Exercicío 063..

---
+++ 063.py
@@ -0,0 +1,8 @@
+marca = 'asus'
+if marca == 'asus':
+    print('True')
+else:
+    print('False')
+
+# A maioria dos Testes Condicionais compara o valor atual de uma variável com um valor de interesse.
+# Neste caso o Teste Condicional verificou se o valor (utilizando ==)de uma varável é igual ao valor de interesse, por isso o resultado foi True.
