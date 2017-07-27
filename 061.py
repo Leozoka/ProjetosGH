aef4b3684421d178bae33f93dda5240d5628344c
Leozoka <leo.neves8@hotmail.com>
2017-07-26 22:09:37 -0300

    Exercicío 061..

---
+++ 061.py
@@ -0,0 +1,9 @@
+listas = ['uno', 'palio', 'opala']
+for lista in listas:
+    if lista == 'fusca':
+        print(lista.upper())
+    else:
+        print(lista.lower())
+
+# A instrução if de Python permite que você examixe o atual estado de um programa.
+# Este conceitos para listas, serve para que você possa escrever um Loop for que atenda mais itens.
