c48a70f2129229bd69fc1e56b8a7329f7d76a3c7
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:52:53 -0300

    Exercicío 066..

---
+++ 066.py
@@ -0,0 +1,6 @@
+dsa = 'razer'
+if dsa != 'multilaser':
+    print('Não quero está marca!')
+
+# Quando você deseja determinar se dois valores não são iguais, você pode combinar um ponto de exclamação e um sinal de igual (!=).
+# A linha compara a variável (dsa), com o valor informado 'multilaser'. Se os dois valores não coincidem, o Python retorna True, caso não coincidirem retorna False.
