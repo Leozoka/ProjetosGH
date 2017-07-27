27feb56ad5abc9d813a2b66bc2d904fa32f46f05
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:57:10 -0300

    Exercicío 070..

---
+++ 070.py
@@ -0,0 +1,9 @@
+idade1 = '32'
+idade2 = '28'
+if idade1 >= '31' and idade2 >= '31':
+    print('True!')
+else:
+    print('False!')
+# Para verificarmos se as duas condições são True, utilizamos a palavra (and), para combinar os testes.
+# Se cada teste passar, o conjunto avalia como True, se qualquer falha ou se ambos os testes falharem a avaliação é False.
+# O teste da esquerda passou, mas o da direita falha, com isso o programa nos devolve False.
