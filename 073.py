a0894bbb0c45a8dc9322eb5c65554f461192f9d7
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:59:07 -0300

    Exercicío 073..

---
+++ 073.py
@@ -0,0 +1,3 @@
+# 73 - Explique a diferença do AND e OR.
+# Com and as condições só são True se as duas condições passarem no teste, caso contrário irá ser False.
+# No or é contrário de and, ou seja, se uma ou ambas condições passarem o resultado será True, e se as duas condições falharem será False.
