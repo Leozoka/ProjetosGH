f4eb3966d76390e0a5f25f491f89e185d77194a5
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:59:36 -0300

    Exercicío 074..

---
+++ 074.py
@@ -0,0 +1,6 @@
+perifericos = ['mouse', 'monitor', 'fone']
+'mouse' in perifericos
+
+# Para descobrir se um valor específico já está em uma lista, use a palavra in.
+# in diz ao Python para verificar se a existência da palavra buscada.
+# Este comando é útil, pois pode criar uma lista de valores e verificar facilmente se o valor que você está buscando corresponde a um dos valores da lista.
