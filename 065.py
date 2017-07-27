97c28ae1bec7d50669125c2ad8aef69345eaeaff
Leozoka <leo.neves8@hotmail.com>
2017-07-26 22:12:22 -0300

    Exercicío 065..

---
+++ 065.py
@@ -0,0 +1,8 @@
+marca = 'Samsung'
+if marca.lower() == 'samsung':
+    print('True')
+else:
+    print('False')
+
+# Você pode converter o valor de uma variável para minúsculas, este retornaria True, porque o teste é insensível a maiúsculas e minúsculas.
+# A função lower() não muda o valor que foi originalmente armazenado na variável.
