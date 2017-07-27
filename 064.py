407d8aac91987dd5a922601b2b2448b4dc79c0a3
Leozoka <leo.neves8@hotmail.com>
2017-07-26 22:11:41 -0300

    Exercicío 064..

---
+++ 064.py
@@ -0,0 +1,8 @@
+marca = 'asus'
+if marca == 'samsung':
+    print('True')
+else:
+    print('False')
+
+# Verificou-se a variável, usando o duplo igual (==), caso este operador retorne True, os valores no lado esquerdo e direito do operador são iguais.
+# Se não forem iguais eles nos devolve dizendo que é False. (Como acabou de acontecer no exemplo acima).
