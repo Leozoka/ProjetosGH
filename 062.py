bdad59f9714767114562a9d244bdfefd5b3eb27c
Leozoka <leo.neves8@hotmail.com>
2017-07-26 22:10:34 -0300

    Exercicío 062..

---
+++ 062.py
@@ -0,0 +1,3 @@
+# 62 - Explique o que são Testes Condicionais.
+# Em cada afirmação if é uma expressão que pode ser avaliada como True ou False e é chamado de Teste Condicional.
+# Python usa os valores True ou False para decidir em uma instrução if deve ser executado.
