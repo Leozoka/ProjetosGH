29e045346cb1bdb45e47065671e2720d0081964a
Leozoka <leo.neves8@hotmail.com>
2017-07-27 16:56:17 -0300

    Exercicío 069..

---
+++ 069.py
@@ -0,0 +1,7 @@
+idade = '17'
+if idade >= '17':
+    print('Menor de idade!')
+else:
+    print('Maior de idade!')
+# Utilizei apenas uma comparação (>=), mas também pode-se utilizar (<=).
+# Cada comparação matemática pode ser usada como parte de uma declaração if, lhe ajuda a detectar as condições de interesse.
