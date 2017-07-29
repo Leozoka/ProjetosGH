9a9bca68d29f63a34a130e8a1c9b65b9d3faad07
Leozoka <leo.neves8@hotmail.com>
2017-07-28 23:24:09 -0300

    Exercicío 077..

---
+++ 077.py
@@ -0,0 +1,4 @@
+# 77 - Explique sobre expressões Booleanas.
+# Um valor Booleano é True or False, assim como o valor de um Teste Condicional ter sido avaliado.
+# Os valores Booleanos são usados para acompanhar certas condições, como se um jogo está sendo executado ou se um usuário pode editar determinado conteúdo num Website.
+# Os valores Booleanos fornecem uma maneira eficiente de rastrear o estado de um programa, ou uma condição que é importante em seu programa.
