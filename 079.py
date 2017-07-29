739ae8ee0b8b0c3a53dd6066a754966326c9a99f
Leozoka <leo.neves8@hotmail.com>
2017-07-28 23:30:06 -0300

    Exercicío 079..

---
+++ 079.py
@@ -0,0 +1,10 @@
+idade = 16
+if idade < 18:
+    print('Você ainda não pode votar!')
+else:
+    print('Você pode votar!')
+
+# Se o Teste Condicional avalia True, o Python executa o código seguindo a instrução if.
+# se o Teste avaliar False, Python ignora o código que segue o if declaração.
+# A indentação desempenhao mesmo papel nas declarações if como ocorreu nos Loops. Todas as linhas recuadas após a declaração if serão executadas se o Teste passar.
+# Com os blocos de linhas recuadas será ignorado se o teste não for feito.
