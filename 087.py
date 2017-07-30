28c2b41f13e288102b770febb929eb9e0f399d8e
Leozoka <leo.neves8@hotmail.com>
2017-07-29 23:04:09 -0300

    Exercicío 087..

---
+++ 087.py
@@ -0,0 +1,7 @@
+ingredientes = ['tomate', 'queijo', 'milho']
+for ingrediente in ingredientes:
+    if ingrediente == 'tomate':
+        print('Desculpa, estamos sem tomate no momento.')
+    else:
+        print('Adicionando ' + ingrediente + '.')
+print('\nPizza pronta.')
