cd28faa70c96f52dcdbfdfbe5a9375ff86b08241
Leozoka <leo.neves8@hotmail.com>
2017-07-29 22:59:54 -0300

    Exercicío 084..

---
+++ 084.py
@@ -0,0 +1,11 @@
+ingredientes = ['queijo', 'presunto', 'milho']
+if 'milho' in ingredientes:
+    print('Milho adicionado.')
+if 'queijo' in ingredientes:
+    print('Queijo adicionado.')
+if 'presunto' in ingredientes:
+    print('Presunto adicionado.')
+print('\nIngredientes finalizados!')
+
+# A cadeia if-elif-else é poderosa, mas é apropriado usar quando precisa passar de um teste para passar.
+# Assim que Python encontra um teste que passa, ele ignora o resto dos testes. Permite que você teste uma condição específica (as vezes é importante verificar todas as condições).
