7283efc910bf7cb67d4869b09a409017bf0299b2
Leozoka <leo.neves8@hotmail.com>
2017-07-29 23:05:24 -0300

    Exercicío 088..

---
+++ 088.py
@@ -0,0 +1,7 @@
+ingredientes = []
+if ingredientes:
+    for ingrediente in ingredientes:
+        print('Adicionando ' + ingrediente + '.')
+        print('\nPizza pronta.')
+else:
+        print('Deseja uma pizza simples?')
