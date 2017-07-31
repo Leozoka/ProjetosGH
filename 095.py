726178845a4cdbdfce747671491ddac95df3d49b
Leozoka <leo.neves8@hotmail.com>
2017-07-30 22:11:18 -0300

    Exercicío 095..

---
+++ 095.py
@@ -0,0 +1,7 @@
+game = {'cor': 'azul'}
+print('A cor do personagem é ' + game['cor'] + '.')
+game['cor'] = 'verde'
+print('Agora a cor do personagem é ' + game['cor'] + '.')
+
+# Para modificar um valor, basta escrever a variável com a chave entre colchetes e digitar o valor desejado.
+# Neste exemplo modificamos a cor do personagem.
