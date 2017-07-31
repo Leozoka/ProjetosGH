942e545b957b8a751b1fe333b692e2414f540198
Leozoka <leo.neves8@hotmail.com>
2017-07-30 22:14:09 -0300

    Exercicío 098..

---
+++ 098.py
@@ -0,0 +1,10 @@
+linguagens = {'ling_1': 'python',
+    'ling_2': 'c++',
+    'ling_3': 'java',
+    }
+print('Minha linguagem favorita é: ' +
+    linguagens['ling_1'].title() +
+    '.')
+
+# Quebramos o dicionários em variás linhas, cada chave é o uma linguagem. Quando você sabe que você precisará de mais de uma linha para definir um dicionário, pressione Enter após o suporte de abertura. Em seguida, recorra a próxima linha um nível (04 espaços) e escreva o primeiro par chave-valor, seguido de uma vírgula.
+# Este exemplo também mostra como você pode dividir uma declaração de impressão longa em várias linhas.
