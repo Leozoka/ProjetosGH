3143c11f8835b95325411ca10b299f3f2225fd76
Leozoka <leo.neves8@hotmail.com>
2017-07-30 22:10:23 -0300

    Exercicío 093..

---
+++ 093.py
@@ -0,0 +1,8 @@
+game = {'personagem': 'alto', 'cor': 'azul'}
+print(game)
+game['comando_1'] = 'chuta'
+game['comando_2'] = 'defende'
+print(game)
+
+# Para adicionar um novo valor, utiliza o nome do dicionário seguido pela nova chave entre colchetes junto do novo valor.
+# A versão final contém 04 valores. Python não se preocupa com a ordem em que você armazena cada valor, se preocupa apenas com a conexão entre cada chave e seu valor.
