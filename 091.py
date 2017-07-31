6c3edd5db8f6dd9af8ab6d062fcfeace5d9d108f
Leozoka <leo.neves8@hotmail.com>
2017-07-30 22:02:08 -0300

    Exercicío 091..

---
+++ 091.py
@@ -0,0 +1,5 @@
+# 91 - Explique o que são dicionários.
+# Um dicionário em Python é uma coleção de pares de valores-chave. Cada chave está conectada a um valor e você pode usar uma chave para acessar o valor. Pode usar qualquer objeto que você possa criar no Python como um valor.
+# Dicionários permitem que você conecte peças relacionadas, como podem armazenar uma quantidade quase ilimitada de informações, podemos fazer um loop dos dados em um dicionário.
+# Podemos aninhar dicionários dentro de listas, listas em dicionários e até dicionário em dicionários.
+# Compreender os dicionáros permite modelar uma variedade de objetos do mundo real com mais precisão.
