bdcb9eb20e48c7ad255a720f8fdef38f6def34e2
Leozoka <leo.neves8@hotmail.com>
2017-07-30 22:13:31 -0300

    Exercicío 097..

---
+++ 097.py
@@ -0,0 +1,8 @@
+asd = {'letra': 'c', 'número': 8}
+print(asd)
+del asd['letra']
+print(asd)
+
+# Quando você não precisa mais de um valor armazenado em um dicionário, pode usar a declarção del para remover o valor.
+# Tudo que precisa é o nome do dicionário e a chave que você deseja remover.
+# Esteja ciente de que o valor excluído é removido permanentemente.
