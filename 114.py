d487e64ae9e214a4beef933cc226cf47a24987d1
Leoander <leo.neves8@hotmail.com>
2017-08-06 18:42:47 -0300

    Exercicío 114..

---
+++ 114.py
@@ -0,0 +1,5 @@
+#Nesse exemplo vamos pedir para que o usuário digite seu nome e vamos deixar uma mensagem para ele.
+#A variável salva o nome do usuário, depois é so concatenar.
+#Para deixar mais organizado podemos deixar um espaço após nosso prompt.
+nome = input('Qual é o seu nome? ')
+print('Prazer em te conhecer, ' + nome + ('!'))
