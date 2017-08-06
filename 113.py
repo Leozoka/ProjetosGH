0ab7762baad35151eb8da75bd7faab54a2818567
Leoander <leo.neves8@hotmail.com>
2017-08-06 18:35:00 -0300

    Exercicío 113..

---
+++ 113.py
@@ -0,0 +1,4 @@
+#A função input() faz com que o usuário digite um texto. Python recebe a entrada do usuário, ele armazena em uma variável para melhorar o seu trabalho.
+#Nesse exemplo pedimos ao usuáro para digitar algo, em seguida essa mensagem retorna ao usuário.
+frase = input('Digite algo: ')
+print(frase)
