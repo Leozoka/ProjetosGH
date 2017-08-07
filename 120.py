739c34bdf96540e581e482920a6acd8c4b338b3c
Leoander <leo.neves8@hotmail.com>
2017-08-07 15:57:09 -0300

    Exercicío 120..

---
+++ 120.py
@@ -0,0 +1,7 @@
+numeros = 1
+while numeros <= 5:
+    print(numeros)
+    numeros += 1
+#Você pode usar o loop while para contar uma série de números.
+#Começamos contando a partir de 1, o loop while é configurado para continuar funcionando enquanto for menor ou igual a 5.
+#O valor dentro do loop imprime o valor atual, e em seguida adiciona 1 a esse valor com numeros +=1.
