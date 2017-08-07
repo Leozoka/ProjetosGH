03d9cc22854e1942d1859a786e54626ecf7f764b
Leoander <leo.neves8@hotmail.com>
2017-08-07 15:36:57 -0300

    Exercicío 118..

---
+++ 118.py
@@ -0,0 +1,5 @@
+print(10 % 3)
+print(100 % 5)
+print(415 % 31)
+#O operador módulo(%) divide um número por outro e retorna o restante (resto, sobra).
+#O operador não lhe diz quantas vezes um número se encaixa em outro, apenas diz o restante.
