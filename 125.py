8bc952a646794e9624cc3d2cf7a1aff79e11d516
Leoander <leo.neves8@hotmail.com>
2017-08-09 14:51:17 -0300

    Exercicío 125..

---
+++ 125.py
@@ -0,0 +1,5 @@
+num = 2
+while num <= 5:
+    print(num)
+#Todos os loops precisam de uma maneira para não ser infinitos.
+#Se você não digitar a linha  num += 2, o programa reconhcerá True e o loop while será executado pra sempre.
