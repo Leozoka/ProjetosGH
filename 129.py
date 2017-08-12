3807c82f8380700650076f3125404f7524583cbd
Leoander <leo.neves8@hotmail.com>
2017-08-12 20:38:07 -0300

    Exercicío 129..

---
+++ 129.py
@@ -0,0 +1,5 @@
+def usuario_1():
+    print('Olá!')
+usuario_1()
+#def informa a Python que você está definindo uma função. Esta definição da função, informa ao Python o nome da função e se aplicável, qual o tipo de informação que a função precisa para fazer seu trabalho.
+#Os parênteses possuem essa informação.
