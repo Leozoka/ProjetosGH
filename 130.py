033c4a43cd65914d8c357585fab7858994dc60c2
Leoander <leo.neves8@hotmail.com>
2017-08-12 20:56:27 -0300

    Exercicío 130..

---
+++ 130.py
@@ -0,0 +1,5 @@
+def usuario_1(nome):
+    print('Olá, ' + nome.title() + '!')
+usuario_1('fulano')
+#Para modificar a função, basta adicionar algum nome para usuário na definição da função def.
+#Depois disso a função passa a aceitar qualquer nome de usuário que você colocar.
