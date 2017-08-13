d7252ebc3a501a746382d0f2373db2c34152468a
Leoander <leo.neves8@hotmail.com>
2017-08-13 18:50:18 -0300

    Exercicío 135..

---
+++ 135.py
@@ -0,0 +1,6 @@
+def animal(animal_tipo, animal_nome):
+    print('Tenho um ' + animal_tipo + '.')
+    print('Meu ' + animal_tipo + ', se chama ' + animal_nome.title() + '.')
+animal()
+#Quando você fornece menos ou mais argumentos do que uma função precisa, ocorre um erro.
+#Python nos informa onde está o erro e quais funções falta informar.
