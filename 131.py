a7aaf22722de1a9799b504ad543c7eacc5b2a594
Leoander <leo.neves8@hotmail.com>
2017-08-12 21:27:15 -0300

    Exercicío 131..

---
+++ 131.py
@@ -0,0 +1,7 @@
+def animal(animal_tipo, animal_nome):
+    print('Tenho um ' + animal_tipo.title() + '.')
+    print('Meu ' + animal_tipo.title() + ', se chama ' + animal_nome.title() + '.')
+animal('cachorro', 'teodoro')
+#Esta função precisa de um tipo de animal e o nome dele.
+#Depois que fornecemos as informções elas são armazenadas cada uma de acordo com o seu parâmetro e o seu argumento.
+#Depois são usados para exibir as informações sobre o animal descrito.
