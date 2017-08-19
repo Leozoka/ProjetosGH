9f76cf33be37b60557dc59c84c97a55966205aaf
Leoander <leo.neves8@hotmail.com>
2017-08-19 16:25:50 -0300

    Exercicío 147..

---
+++ 147.py
@@ -0,0 +1,5 @@
+#import nome_módulo - para importar todas as funções.
+#from nome_módulo import * - também importa todas as funções.
+#from nome_módulo import nome_função - Desse modo você pode importar funções específicas no módulo.
+#from nome_módulo import nome_função as mp- Se o nome de uma função que você está importando pode entrar em conflito com um nome existente no seu programa ou se o nome da função for longo, você pode usar um mp().
+#import nome_módulo as l - Você também pode fornecer um nome para o módulo, assim sendo, um módulo um curto.
