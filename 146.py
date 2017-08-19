3bd638a3eaf5ef87cf4af831607ff752d4e22d3b
Leoander <leo.neves8@hotmail.com>
2017-08-19 15:46:32 -0300

    Exercicío 146..

---
+++ 146.py
@@ -0,0 +1,6 @@
+import pizza
+pizza.fazendo_pizza(15, 'Queijo', 'Catupiry')
+pizza.fazendo_pizza(20, 'Presunto e Mussarela', 'Chocolate')
+#Para começar a importar funções, primeiro precisamos criar um módulo. Um módulo é um arquivo que termina em .py que contém o código que deseja importar para o seu programa.
+#Para chamar uma função de um módulo importado, insira o nome do módulo que você importou, seguido do nome da função, separado por um ponto.
+#
